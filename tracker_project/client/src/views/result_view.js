df8ea57e50dff01f2c405d588ae61f814362920e
Ruairidh Holwill <roore@hotmail.co.uk>
2019-05-27 14:32:15 +0100

    tracker details now displayed

--- tracker_project/client/src/views/result_view.js
+++ tracker_project/client/src/views/result_view.js
@@ -4,20 +4,34 @@ const ResultsView = function(container ) {
     this.container = container;
   }
 
-  ResultsView.prototype.bindEvents = function () {
-      PubSub.subscribe('Booze:saving-goal', (event) => {
-          // create saving goal view
-      })
+ResultsView.prototype.bindEvents = function () {
+    PubSub.subscribe('Booze:saving-goal', (event) => {
+      this.createGoalView(event.detail)
+    })
 
-      PubSub.subscribe('Booze:total-spent-calculated', (event) => {
-        // create saving goal view
-      })
+    PubSub.subscribe('Booze:total-spent-calculated', (event) => {
+      this.createSpentView(event.detail)
+    })
 
-      PubSub.subscribe('Booze:savings-progress', (event) => {
-        // create saving goal view
-      })
+    PubSub.subscribe('Booze:savings-progress', (event) => {
+      this.createOverUnderView(event.detail)
+    })
   }
 
-  
+ResultsView.prototype.createGoalView = function(event) {
+    const goalAmount = document.querySelector('#goal');
+    goalAmount.textContent = `Goal: £${event}`  
+}
+
+ResultsView.prototype.createSpentView = function(event) {
+    const spentAmount = document.querySelector('#total-spent');
+    spentAmount.textContent = `Spent: £${event}`  
+}
+
+ResultsView.prototype.createOverUnderView = function(event) {
+    const OverUnderAmount = document.querySelector('#over-under');
+    OverUnderAmount.textContent = `Budget Left: £${event}`
+    }  
+
 
   module.exports = ResultsView;
