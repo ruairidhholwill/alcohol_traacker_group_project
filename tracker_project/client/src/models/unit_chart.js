6b9c9363d8d1b1b2cb56d20043352cc16e3291a2
Diego Diniz da Silva <diegodinizz@gmail.com>
2019-05-29 21:00:34 +0100

    unit chart model cleaned up

--- tracker_project/client/src/models/unit_chart.js
+++ tracker_project/client/src/models/unit_chart.js
@@ -1,65 +1,58 @@
 PubSub = require('../helpers/pub_sub');
 
 const UnitChart = function(container){
-    this.container = container;
-    this.units = 0
-    this.colour = "yellow"
-    this.statement = "The NHS recommend the max alcohol unit consumption(student exemptions apply) as"
-}
+  this.container = container;
+  this.units = 0
+  this.colour = "yellow"
+  this.statement = "The NHS recommend the max alcohol unit consumption(student exemptions apply) as"
+};
 
 
 UnitChart.prototype.bindEvents = function(){
-    PubSub.subscribe('Results:total-units-calculated', (event)=>{
-        console.log('units passed', event.detail)
-        units = event.detail
-        this.render(units);
-    })
-}
+  PubSub.subscribe('Results:total-units-calculated', (event)=>{
+    units = event.detail
+    this.render(units);
+  })
+};
 
 UnitChart.prototype.changeSegmentColour = function(){
-    if (units <= 15){
-        this.colour = "green"
-    }
-    else if(units > 15 && units < 35){
-        this.colour = "orange"
-    }
-    else if(units > 35 && units < 56){
-        this.colour = "red"
-    }
-}
-
-
-
+  if (units <= 15){
+    this.colour = "green"
+  }
+  else if(units > 15 && units < 35){
+    this.colour = "orange"
+  }
+  else if(units > 35 && units < 56){
+    this.colour = "red"
+  }
+};
 
 UnitChart.prototype.render = function(units){
 
+  this.changeSegmentColour();
 
-    this.changeSegmentColour();
+  var chart = new CanvasJS.Chart("unitChartContainer", {
 
-    console.log("inside unit function",units)
+    animationEnabled: true,
+    title: {
+      text: "Alcohol Unit Tracker"
 
-    var chart = new CanvasJS.Chart("unitChartContainer", {
+    },
+    data: [{
+      type: "pie",
+      startAngle: -90,
+      indexLabel: "{label} {y}",
 
-        animationEnabled: true,
-        title: {
-            text: "Alcohol Unit Tracker"  
-                    
-        },
-        data: [{
-            type: "pie",
-            startAngle: -90,
-            indexLabel: "{label} {y}",
-            
 
-            dataPoints: [
-                {label:"Consumed alcohol units", y: `${units}`,  color: `${this.colour}`},
-                {y:56, label:`${this.statement}`,   color: "beige"}
-               
-            ]
-        }]
-    });
-    chart.render()
+      dataPoints: [
+        {label:"Consumed alcohol units", y: `${units}`,  color: `${this.colour}`},
+        {y:56, label:`${this.statement}`,   color: "beige"}
 
-}
+      ]
+    }]
+  });
+  chart.render()
+
+};
 
 module.exports = UnitChart;
