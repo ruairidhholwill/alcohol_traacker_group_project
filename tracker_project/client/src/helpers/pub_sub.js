7554fa5a361090996fa6c20ce5fad45c16f6711b
Diego Diniz da Silva <diegodinizz@gmail.com>
2019-05-29 20:11:32 +0100

    PubSub cleaned up

--- tracker_project/client/src/helpers/pub_sub.js
+++ tracker_project/client/src/helpers/pub_sub.js
@@ -1,16 +1,13 @@
 const PubSub = {
-    publish: function(channel, payload) {
-        console.log(`Published on ${channel}`)
-        console.log('payload', payload)
-        const event = new CustomEvent(channel, {
-            detail: payload
-        });
-        document.dispatchEvent(event);
-    },
-    subscribe: function(channel, callback) {
-        console.log(`Subscribed on ${channel}`)
-        document.addEventListener(channel, callback);
-    }
+  publish: function(channel, payload) {
+    const event = new CustomEvent(channel, {
+      detail: payload
+    });
+    document.dispatchEvent(event);
+  },
+  subscribe: function(channel, callback) {
+    document.addEventListener(channel, callback);
+  }
 };
 
 module.exports = PubSub;
