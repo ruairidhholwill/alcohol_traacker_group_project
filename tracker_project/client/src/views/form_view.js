a28480f34d116bb1becbc6d4e736f1f1977ed7ab
Ruairidh Holwill <roore@hotmail.co.uk>
2019-05-29 11:59:40 +0100

    drink sizes now reset properly

--- tracker_project/client/src/views/form_view.js
+++ tracker_project/client/src/views/form_view.js
@@ -32,10 +32,10 @@ FormView.prototype.bindEvents = function () {
       } else {
         newDrink = this.createDrinkInfo(event.target);
         PubSub.publish('BoozeFormView:booze-submitted', newDrink);
-        // console.log(newDrink)
         event.target.reset()//empties the text fields.
         // const unit = new UnitHelper(newDrink.drinkType, newDrink.drinkSize);
         // console.log(unit.sizeToUnits())
+        this.renderDrinkSizeDefaults('beer')
       }
 
 
